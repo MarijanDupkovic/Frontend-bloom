2e751392b397920f454d3328b1a2ee97b1ab888b
MarijanDupkovic <info@marijandupkovic.com>
2024-01-27 22:01:15 +0100

    MD created sign up Basis

--- bloom-frontend/src/app/app.routes.ts
+++ bloom-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { VorgabenComponent } from './manuals/vorgaben/vorgaben.component';
 import { ContentComponent } from './components/content/content.component';
 import { LoginComponent } from './components/Auth/login/login.component';
+import { SignUpComponent } from './components/Auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: ContentComponent, pathMatch: 'full' },
     { path: 'vorgaben' , component: VorgabenComponent },
     { path: 'signin' , component: LoginComponent },
+    { path: 'signup' , component: SignUpComponent },
 ];
