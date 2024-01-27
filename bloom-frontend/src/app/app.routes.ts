bb40d89657d72dc3e48ba0a5decffeae44695dee
MarijanDupkovic <info@marijandupkovic.com>
2024-01-27 16:04:29 +0100

    Createt Login Basis

--- bloom-frontend/src/app/app.routes.ts
+++ bloom-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { VorgabenComponent } from './manuals/vorgaben/vorgaben.component';
 import { ContentComponent } from './components/content/content.component';
+import { LoginComponent } from './components/Auth/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: ContentComponent, pathMatch: 'full' },
     { path: 'vorgaben' , component: VorgabenComponent },
+    { path: 'signin' , component: LoginComponent },
 ];
