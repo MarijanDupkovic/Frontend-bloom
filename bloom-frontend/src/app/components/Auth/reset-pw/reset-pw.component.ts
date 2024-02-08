e14f59c793b4861dfa9efa6e8c1969cb57a3ef44
MarijanDupkovic <info@marijandupkovic.com>
2024-02-08 10:57:41 +0100

    Added_pw_reset_mail_and_pw_reset

---
+++ bloom-frontend/src/app/components/Auth/reset-pw/reset-pw.component.ts
@@ -0,0 +1,101 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../../environtments/environtment';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reset-pw',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './reset-pw.component.html',
+  styleUrl: './reset-pw.component.scss'
+})
+export class ResetPwComponent {
+  password: string = '';
+  password2: string = '';
+  hide: boolean = true;
+  hide2: boolean = true;
+  send: boolean = false;
+  login_failed: boolean = false;
+  fail_message: string = '';
+  message = ``;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { 
+    
+  }
+
+  ngOnInit() {
+    
+    this.addEnterListener();
+  }
+
+  togglePWField(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.hide = !this.hide;
+  }
+  togglePWField2(e: Event){
+    e.preventDefault();
+    e.stopPropagation();
+    this.hide2 = !this.hide2;
+  }
+
+  addEnterListener() {
+   addEventListener('keydown', (e: KeyboardEvent) => {
+   
+      if(e.key === 'Enter') {
+        e.preventDefault();
+        e.stopPropagation();
+        if(this.password !== '' && this.password2 !== '') {
+           this.resetPassword();
+        }else this.setErrorMessage('Passwort darf nicht leer sein', {error: 'Bitte fülle alle Felder aus!'}  );
+      }
+    
+  }
+  );
+  }
+
+  async resetPassword() {
+      this.route.params.subscribe(async params => {
+        const url = environment.baseUrl + '/resetPassword/' + params['token'] + '/';
+        const data = {
+          pw1: this.password,
+          pw2: this.password2
+        };
+        let response = await lastValueFrom(this.http.post(url, data));
+        if((response as any).message === 'Passwort stimmt nicht mit Passwort2 überein!') {
+          this.setErrorMessage('Passwort stimmt nicht mit Passwort2 überein!', response);
+        } else {
+          this.setErrorMessage('Passwort erfolgreich geändert! Du wirst zum Login weitergeleitet', response);
+          setTimeout(() => {
+            window.location.href = '/login';
+          }, 3000);
+        }
+      });
+    
+  }
+  setErrorMessage(message: string, error: any) {
+    this.login_failed = true;
+    this.fail_message = error.error;
+    this.message = message;
+    this.resetErrorMessage();
+  }
+
+  resetErrorMessage() {
+    setTimeout(() => {
+
+      this.login_failed = false;
+      this.fail_message = '';
+      this.message = '';
+      this.send = false;
+      const passwordField = document.getElementById('passwordField') as HTMLInputElement;
+      passwordField.value = '';
+      const passwordField2 = document.getElementById('passwordField2') as HTMLInputElement;
+      passwordField2.value = '';
+    }, 3000);
+
+  }
+}
--- bloom-frontend/src/app/components/Auth/send-pw-reset/send-pw-reset.component.ts
+++ bloom-frontend/src/app/components/Auth/send-pw-reset/send-pw-reset.component.ts
@@ -1,12 +1,91 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../../environtments/environtment';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-send-pw-reset',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './send-pw-reset.component.html',
   styleUrl: './send-pw-reset.component.scss'
 })
 export class SendPwResetComponent {
+  email: string = '';
+  password: string = '';
+  hide: boolean = true;
+  send: boolean = false;
+  login_failed: boolean = false;
+  fail_message: string = '';
+  message = ``;
 
+  constructor(private http: HttpClient, private router: Router) { this.addEnterListener()}
+
+  addEnterListener() {
+    addEventListener('keydown', (e: KeyboardEvent) => {
+    
+       if(e.key === 'Enter') {
+        e.preventDefault();
+        e.stopPropagation();
+         if(this.email !== '') {
+           this.getResetPasswordMail();
+         }else this.setErrorMessage('Bitte fülle alle Felder aus!', {error: 'Bitte fülle alle Felder aus!'}  );
+       }
+     
+   }
+   );
+   }
+  async getResetPasswordMail() {
+    this.send = true;
+    const url = environment.baseUrl + '/resetPasswordMail/';
+    const body = {
+      email: this.email,
+    };
+    let response: any;
+    try {
+      response = await lastValueFrom(this.http.post(url, body));
+      this.setErrorMessage('Email wurde versendet!', response.message);
+      setTimeout(() => {
+        this.router.navigateByUrl('/login');
+      }, 3000);
+    } catch (e) {
+      let error: any = e;
+
+      if (error.status == 404) {
+        this.setErrorMessage(' Benutzer mit dieser Emailadresse nicht vorhanden.', error);
+      }
+      else if (error.status == 400) {
+        this.setErrorMessage(' Bitte gib eine Emailadresse ein.', error);
+
+      } else {
+        this.setErrorMessage('Email wurde versendet!', error);
+        setTimeout(() => {
+          this.router.navigateByUrl('/login');
+        }, 3000);
+      }
+    }
+
+
+  }
+  setErrorMessage(message: string, error: any) {
+    this.login_failed = true;
+    this.fail_message = error.error;
+    this.message = message;
+    this.resetErrorMessage();
+  }
+
+  resetErrorMessage() {
+    setTimeout(() => {
+      this.login_failed = false;
+      this.fail_message = '';
+      this.message = '';
+      this.send = false;
+      const emailField = document.getElementById('emailField') as HTMLInputElement;
+      emailField.value = '';
+    }, 3000);
+
+  }
 }
