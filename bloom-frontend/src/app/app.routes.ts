eb7490c9d1634b82021ee3599651ff484cee26d8
MarijanDupkovic <info@marijandupkovic.com>
2024-02-09 20:21:04 +0100

    Added_user_activation

--- bloom-frontend/src/app/app.routes.ts
+++ bloom-frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/Auth/login/login.component';
 import { SignUpComponent } from './components/Auth/sign-up/sign-up.component';
 import { SendPwResetComponent } from './components/Auth/send-pw-reset/send-pw-reset.component';
 import { ResetPwComponent } from './components/Auth/reset-pw/reset-pw.component';
+import { UserActivationComponent } from './components/Auth/user-activation/user-activation.component';
 
 export const routes: Routes = [
     { path: '', component: ContentComponent, pathMatch: 'full' },
@@ -12,5 +13,7 @@ export const routes: Routes = [
     { path: 'signin' , component: LoginComponent },
     { path: 'signup' , component: SignUpComponent },
     { path: 'reset-mail' , component: SendPwResetComponent },
-    { path: 'reset-pw' , component: ResetPwComponent },
+    { path: 'reset-pw/:token' , component: ResetPwComponent },
+    { path: 'activate/:token', component: UserActivationComponent },
+
 ];
