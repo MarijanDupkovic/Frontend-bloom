158da250d54431014366426decb991fb14dacb2d
MarijanDupkovic <info@marijandupkovic.com>
2024-02-15 16:18:10 +0100

    Recording_preview

---
+++ bloom-frontend/src/app/services/Stream/user-media.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import * as bodyPix from '@tensorflow-models/body-pix';
+import { ModelConfig } from '@tensorflow-models/body-pix/dist/body_pix_model';
+import { BodyPixArchitecture } from '@tensorflow-models/body-pix/dist/types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserMediaService {
+
+ 
+
+  constructor() { }
+
+ 
+
+
+
+}
+
