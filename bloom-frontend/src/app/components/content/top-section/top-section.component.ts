3f0289d69252ddd00daf281eef1dd29bec9ffe1f
MarijanDupkovic <info@marijandupkovic.com>
2024-02-19 23:53:59 +0100

    A_lot_of_styling_all_over

--- bloom-frontend/src/app/components/content/top-section/top-section.component.ts
+++ bloom-frontend/src/app/components/content/top-section/top-section.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-section',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top-section.component.html',
   styleUrl: './top-section.component.scss'
 })
 export class TopSectionComponent {
+  videos = [
+    "assets/videos/video1.mp4",
+    "assets/videos/video2.mp4",
+    "assets/videos/video3.mp4"
+  ];
+  currentVideoIndex = 0;
 
+  nextVideo() {
+    this.currentVideoIndex = (this.currentVideoIndex + 1) % this.videos.length;
+  }
 }
