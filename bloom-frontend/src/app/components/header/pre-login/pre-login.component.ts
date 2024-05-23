96fb724205fced3f9b45291da7268fa26d18e717
MarijanDupkovic <info@marijandupkovic.com>
2024-05-23 13:34:30 +0200

    added_overflow_hidden_for_overlay

--- bloom-frontend/src/app/components/header/pre-login/pre-login.component.ts
+++ bloom-frontend/src/app/components/header/pre-login/pre-login.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { UserService } from '../../../services/profile/user.service';
-
+import { Renderer2, RendererFactory2 } from '@angular/core';
 @Component({
   selector: 'app-pre-login',
   standalone: true,
@@ -13,9 +12,17 @@ import { UserService } from '../../../services/profile/user.service';
 export class PreLoginComponent {
   isOpen: boolean = false;
   @Input() active:boolean = false;
-
+  private renderer: Renderer2;
+  constructor(private rendererFactory: RendererFactory2) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+  }
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
   }
 }
